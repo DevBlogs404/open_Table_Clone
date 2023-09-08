3ccc99178038223f4dc70182c992b82384f4a621
shubham <hoodashubham92@gmail.com>
2023-09-08 20:27:14 +0530

    feature: User SignUp functionality with validation Implemented

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,89 @@
+import { NextApiRequest,NextApiResponse } from "next"
+import validator from 'validator';
+import { PrismaClient } from "@prisma/client";
+import bcrypt from 'bcrypt'
+import * as jose from 'jose'
+
+const prisma = new PrismaClient()
+
+export default async function handler(req:NextApiRequest,res:NextApiResponse) {
+
+    const {firstName,lastName,email,phone,city,password} = req.body
+
+    const showError:string[]  = [];
+
+    const validationSchema = [
+        {
+            valid: validator.isLength(firstName,{min:1,max:20}),
+            errorMessage: "First name is invalid"
+        },
+        {
+            valid: validator.isLength(lastName,{min:1,max:20}),
+            errorMessage: "Last name is invalid"
+        },
+        {
+            valid: validator.isEmail(email),
+            errorMessage: "Email is invalid"
+        },
+        {
+            valid: validator.isMobilePhone(phone),
+            errorMessage: "Phone number is invalid"
+        },
+        {
+            valid: validator.isLength(city,{min:1}),
+            errorMessage: "City is invalid"
+        },
+        {
+            valid: validator.isStrongPassword(password),
+            errorMessage: "Password is invalid"
+        },
+    ];
+
+    validationSchema.forEach((check)=>{
+        if(!check.valid){
+            showError.push(check.errorMessage)
+        }
+    })
+
+    if(showError.length){
+        return  res.status(400).json({errorMessage:showError[0]})
+      }
+
+    const getUserWithEmail = await prisma.user.findUnique({
+        where:{
+            email
+        }
+    })
+
+    if(getUserWithEmail){
+        return  res.status(400).json({errorMessage:"Email is already Registered"})
+    }
+
+    const hashedPassword = await bcrypt.hash(password,10)
+
+    const user = await prisma.user.create({
+        data:{
+           first_name:firstName,
+           last_name:lastName,
+           password:hashedPassword,
+           city,
+           phone,
+           email
+        }
+    });
+
+    const alg = "HS256"
+
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET) || "LKNVGESHGVIUBSVKNDSKJCNBIU3ERY812Y43BFU"
+
+    const token = await new jose.SignJWT({email:user.email}).setProtectedHeader({alg}).setExpirationTime("24h").sign(secret)
+
+       if(req.method === "POST"){
+        res.status(200).json({
+            success:true,
+            message:"User SignUp Successful",
+            user,
+            token
+        })
+       }
+}
--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1047,7 +1047,7 @@ export default async function handler(
         last_name: "Harb",
         email: "laith@hotmail.com",
         city: "ottawa",
-        // password: "$2b$10$I8xkU2nQ8EAHuVOdbMy9YO/.rSU3584Y.H4LrpIujGNDtmny9FnLu",
+        password: "$2b$10$I8xkU2nQ8EAHuVOdbMy9YO/.rSU3584Y.H4LrpIujGNDtmny9FnLu",
         phone: "1112223333",
       },
     });
@@ -1058,7 +1058,7 @@ export default async function handler(
         last_name: "Allen",
         email: "josh@hotmail.com",
         city: "toronto",
-        // password: "$2b$10$I8xkU2nQ8EAHuVOdbMy9YO/.rSU3584Y.H4LrpIujGNDtmny9FnLu",
+        password: "$2b$10$I8xkU2nQ8EAHuVOdbMy9YO/.rSU3584Y.H4LrpIujGNDtmny9FnLu",
         phone: "1112223333",
       },
     });
@@ -1069,7 +1069,7 @@ export default async function handler(
         last_name: "James",
         email: "lebron@hotmail.com",
         city: "niagara",
-        // password: "$2b$10$I8xkU2nQ8EAHuVOdbMy9YO/.rSU3584Y.H4LrpIujGNDtmny9FnLu",
+        password: "$2b$10$I8xkU2nQ8EAHuVOdbMy9YO/.rSU3584Y.H4LrpIujGNDtmny9FnLu",
         phone: "1112223333",
       },
     });
@@ -1080,7 +1080,7 @@ export default async function handler(
         last_name: "Marksom",
         email: "cassidy@hotmail.com",
         city: "toronto",
-        // password: "$2b$10$I8xkU2nQ8EAHuVOdbMy9YO/.rSU3584Y.H4LrpIujGNDtmny9FnLu",
+        password: "$2b$10$I8xkU2nQ8EAHuVOdbMy9YO/.rSU3584Y.H4LrpIujGNDtmny9FnLu",
         phone: "1112223333",
       },
     });
